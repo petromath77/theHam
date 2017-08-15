e8aaf707ac5fad2140efc35e6646e8b4aedc8023
petromath77 <petromath77@gmail.com>
2017-08-15 17:35:21 +0300

    footer&small_changes

--- app/js/common.js
+++ app/js/common.js
@@ -2,12 +2,21 @@ $(function() {
 
 	// Custom JS
 
-	$(".mix_wrap").mixItUp();
+	$('.mix_wrap').mixItUp();
 
 	$('.work_menu li').on('click', function(){
 		$('.work_menu li').removeClass('active');
 		$(this).addClass('active');
 	});
-
+	$(window).scroll('on', function(event){
+			if(window.pageYOffset > 50 ){
+				$('.head_row').css("background-color", "rgba(0, 0, 0, 0.9)");
+			}else{
+				$('.head_row').css("background-color", "rgba(0, 0, 0, 0.6)");
+			}
+		})
 
 });
+	
+		
+	
