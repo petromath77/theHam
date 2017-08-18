a4a9ebd52f4e34167678768129406e2300293022
petromath77 <petromath77@gmail.com>
2017-08-18 17:25:32 +0300

    menu_nav_media_pt1

--- app/js/common.js
+++ app/js/common.js
@@ -8,6 +8,15 @@ $(function() {
 		$('.work_menu li').removeClass('active');
 		$(this).addClass('active');
 	});
+
+	//$('.menu_trigger').on('click', function(){
+		//$('.menu_wrap').slideToggle(1000);
+	//});
+
+
+
+
+
 	$(window).scroll('on', function(event){
 			if(window.pageYOffset > 50 ){
 				$('.head_row').css("background-color", "rgba(0, 0, 0, 0.9)");
